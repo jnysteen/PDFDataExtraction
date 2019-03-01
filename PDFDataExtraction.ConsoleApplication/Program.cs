5237bacb05c8fd4893a12241dc351ec2d8e1c0ab
Jesper Nysteen <j@jnysteen.dk>
2019-03-02 00:48:46 +0100

    Initial commit

---
+++ PDFDataExtraction.ConsoleApplication/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace PDFDataExtraction.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = args[0];
+
+            var pdfToTextService = new PDFToTextWrapper(".");
+
+            Console.WriteLine($"Extracting text from PDF...");
+
+            var inputFileWithoutExtension = Path.GetFileNameWithoutExtension(inputFile);
+
+            var outputFile = $"{inputFileWithoutExtension}-extracted-text.xml";
+
+            pdfToTextService.PDFToText(inputFile, outputFile, new string[] { "-bbox-layout" });
+
+
+            Console.WriteLine($"Reading XML file {outputFile}");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Html));
+
+            using (var reader = new StreamReader(outputFile))
+            {
+                var deserializedDoc = (Html)serializer.Deserialize(reader);
+                PrintDocument(deserializedDoc);
+            }
+        }
+
+        static void PrintDocument(Html html)
+        {
+            var pages = html.Body.Doc.Pages;
+
+            foreach(var page in pages)
+            {
+                foreach (var flow in page.Flows)
+                {
+                    foreach (var block in flow.Blocks)
+                    {
+                        foreach (var line in block.Lines)
+                        {
+                            foreach (var word in line.Words)
+                            {
+                                Console.Write($"{word.Text} ");
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PDFDataExtraction/PDFToTextWrapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+// ReSharper disable StringLiteralTypo
+
+namespace PDFDataExtraction
+{
+    public class PDFToTextWrapper
+    {
+        private string _workingDirectory;
+
+        public PDFToTextWrapper(string workingDirectory)
+        {
+            _workingDirectory = workingDirectory;
+        }
+
+        public string PDFToText(string inputFilePath, string outputFilePath, string[] otherArgs)
+        {
+            var otherArgsAsString = string.Join(" ", otherArgs);
+
+            var cmd = $"pdftotext {otherArgsAsString} {inputFilePath} {outputFilePath}";
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+    }
+}
