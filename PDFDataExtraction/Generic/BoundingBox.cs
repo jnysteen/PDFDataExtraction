b622c4ba1f49f79ebca15e21b1b1af88fdff5a8f
Jesper Nysteen <j@jnysteen.com>
2019-09-15 21:50:50 +0200

    Ignore computed properies for boundingbox

--- PDFDataExtraction/Generic/BoundingBox.cs
+++ PDFDataExtraction/Generic/BoundingBox.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace PDFDataExtraction.Generic
 {
     public class BoundingBox
@@ -5,12 +7,18 @@ namespace PDFDataExtraction.Generic
         public Point TopLeftCorner { get; set; }
         public Point BottomRightCorner { get; set; }
 
-        public double Height => BottomRightCorner.Y -TopLeftCorner.Y; 
+        [JsonIgnore]
+        public double Height => BottomRightCorner.Y -TopLeftCorner.Y;
+        [JsonIgnore]
         public double Width => BottomRightCorner.X - TopLeftCorner.X;
-
+        
+        [JsonIgnore]
         public double MinX => TopLeftCorner.X;
+        [JsonIgnore]
         public double MaxX => BottomRightCorner.X;
+        [JsonIgnore]
         public double MinY => TopLeftCorner.Y;
+        [JsonIgnore]
         public double MaxY => BottomRightCorner.Y;
     }
 }
