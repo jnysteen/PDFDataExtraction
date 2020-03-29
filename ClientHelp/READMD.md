6b0445fb0789c11f2159782a49329701676019ae
Jesper Nysteen <j@jysteen.com>
2020-03-29 23:48:48 +0200

    Support client generation with NSwag by using Swagger 2.0 instead of 3.0 (this caused an issue: https://github.com/RicoSuter/NSwag/issues/2419)

--- ClientHelp/READMD.md
+++ ClientHelp/READMD.md
@@ -2,7 +2,9 @@
 
 ## How to generate a client for the PDF Data Extraction Service API
 
-1. Ensure that you have the SDK for .NET Core 2.1 installed (https://dotnet.microsoft.com/download/dotnet-core/2.1)
+1. Ensure that you have the SDK for .NET Core 2.1 installed (https://dotnet.microsoft.com/download/dotnet-core/2.1).
 1. Ensure that the `nswag` CLI tool is installed (https://github.com/RicoSuter/NSwag/wiki/NSwagStudio)
 1. Download the new `swagger.json` file from the PDF Data Extraction Service
 1. Run `nswag run` in this directory
+1. Use the generated client (found in the outputted `.cs` file) to interact with the service! 
+
--- PDFDataExtraction.WebAPI/Extensions/SwaggerDocsExtensions.cs
+++ PDFDataExtraction.WebAPI/Extensions/SwaggerDocsExtensions.cs
@@ -34,7 +34,12 @@ namespace PDFDataExtraction.WebAPI.Extensions
         public static void ConfigureSwaggerUI(this IApplicationBuilder app)
         {
             // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
+            app.UseSwagger(c =>
+            {
+                // TODO HACK To support NSwag client generation, we need to use Swagger JSON 2.0 - not anything never :(
+                //  When this issue is closed, we can upgrade: https://github.com/RicoSuter/NSwag/issues/2419
+                c.SerializeAsV2 = true; 
+            });
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
             // specifying the Swagger JSON endpoint.
