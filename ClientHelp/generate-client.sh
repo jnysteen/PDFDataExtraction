b521cdb82b232347601010ff2457a8bbc336781e
Jesper Nysteen <j@jysteen.com>
2020-03-29 23:46:08 +0200

    cleanup

--- ClientHelp/generate-client.sh
+++
@@ -1,5 +0,0 @@
-# Requires:
-# * The CLI tool nswag to be installed: https://github.com/RicoSuter/NSwag/wiki/CommandLine
-# * The PDF Data Extraction Service swagger.json to be saved from whatever folder this script is run from
-
-nswag openapi2csclient /input:swagger.json /classname:PdfDataExtractionClient /namespace:PdfDataExtraction /output:PdfDataExtractionClient.cs
