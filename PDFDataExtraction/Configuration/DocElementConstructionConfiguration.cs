e6dc36b9b6f26ea86acda96269eb35919e54105d
Jesper Nysteen <j@jysteen.com>
2020-04-02 16:20:52 +0200

    Update default wordDiff to 4

--- PDFDataExtraction/Configuration/DocElementConstructionConfiguration.cs
+++ PDFDataExtraction/Configuration/DocElementConstructionConfiguration.cs
@@ -2,7 +2,7 @@ namespace PDFDataExtraction.Configuration
 {
     public class DocElementConstructionConfiguration
     {
-        public int MaxPixelDifferenceInWordsInTheSameLine { get; set; } = 10;
+        public int MaxPixelDifferenceInWordsInTheSameLine { get; set; } = 4;
         public double WhiteSpaceSizeAsAFactorOfMedianCharacterWidth { get; set; } = 0.2;
     }
 }
