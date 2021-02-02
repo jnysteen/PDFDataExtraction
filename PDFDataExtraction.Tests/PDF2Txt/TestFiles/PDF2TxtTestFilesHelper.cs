c9a3f69e362ebb4e019506912b5aa15e97ec8cc8
Jesper Nysteen <j@jnysteen.com>
2021-02-02 21:23:26 +0100

    Fix tests

--- PDFDataExtraction.Tests/PDF2Txt/TestFiles/PDF2TxtTestFilesHelper.cs
+++ PDFDataExtraction.Tests/PDF2Txt/TestFiles/PDF2TxtTestFilesHelper.cs
@@ -6,14 +6,7 @@ namespace PDFDataExtraction.Tests.PDF2Txt.TestFiles
 {
     public static class PDF2TxtTestFilesHelper
     {
-        public static string SimpleTwoPageDoc => GetTestFilesFolder() + "/simple-two-page-doc.pdf2txt.xml";
+        public static string SimpleTwoPageDoc => TestHelper.GetTestFilesFolder() + "/simple-two-page-doc.pdf2txt.xml";
 
-        public static string GetTestFilesFolder()
-        {
-            var codeBaseUrl = new Uri(Assembly.GetExecutingAssembly().Location);
-            var codeBasePath = Uri.UnescapeDataString(codeBaseUrl.AbsolutePath);
-            var dirPath = Path.GetDirectoryName(codeBasePath);
-            return Path.Combine(dirPath, "PDF2Txt/TestFiles");
-        }
     }
 }
---
+++ PDFDataExtraction.Tests/TestHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace PDFDataExtraction.Tests
+{
+    public static class TestHelper
+    {
+        public static string GetTestFilesFolder()
+        {
+            var codeBaseUrl = new Uri(Assembly.GetExecutingAssembly().Location);
+            var codeBasePath = Uri.UnescapeDataString(codeBaseUrl.AbsolutePath);
+            var dirPath = Path.GetDirectoryName(codeBasePath);
+            var result = Path.Join(dirPath, "TestFiles");
+            return result;
+        }
+    }
+}
